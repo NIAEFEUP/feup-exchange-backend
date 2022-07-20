b98c865c60e762e30760e64dbe0b6f0573d70fe1
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-07-20 11:28:42 +0000

    Added endpoint response for the algorithm execution deadline

--- django/algorithm/views.py
+++ django/algorithm/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.views import status
 
-# Create your views here.
+from .serializers import DeadlineSerializer
+
+class DeadlineView(APIView):
+    """
+    View that fetches the datetime for when the algorithm is scheduled to execute
+    """
+    def get(self, request):
+        """
+        GET method of the dealine fetching view
+        """
+        serializer = DeadlineSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(data=serializer.errors)
